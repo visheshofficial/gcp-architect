cc2399107c10cfcae49b51e767243dbf55f46b06
visheshofficial <21136428+visheshofficial@users.noreply.github.com>
2025-02-18 21:38:05 +0000

    Add Terraform configuration for GCP VM with instance template and static IP support

--- gce/gcp-vm/main.tf
+++ gce/gcp-vm/main.tf
@@ -6,14 +6,66 @@ provider "google" {
 }
 
 # Create a static IP address
-# resource "google_compute_address" "static_ip" {
-#   name   = "my-static-ip"
-#   region = var.region
-# }
+resource "google_compute_address" "static_ip" {
+  count  = var.create_static_ip ? 1 : 0
+  name   = "my-static-ip"
+  region = var.region
+}
+
+# Create an instance template
+resource "google_compute_instance_template" "instance_template" {
+  name         = "my-instance-template"
+  machine_type = var.machine_type
+
+  tags = ["http-server"]
+
+  disk {
+    source_image = "debian-cloud/debian-12"
+    auto_delete  = true
+    boot         = true
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = var.create_static_ip ? google_compute_address.static_ip[0].address : null
+    }
+  }
+
+  metadata_startup_script = <<-EOT
+              #!/bin/bash
+              # sudo su
+              apt update
+              apt install -y apache2
+              # ls /var/www/html
+              # echo "Hello World!"
+              # echo "Hello World!" > /var/www/html/index.html
+              # echo $(hostname)
+              # echo $(hostname -i)
+              # echo "Hello World from $(hostname)"
+              # echo "Hello World from $(hostname) $(hostname -i)"
+              echo "Hello world from $(hostname) $(hostname -i)" > /var/www/html/index.html
+              sudo service apache2 start
+              EOT
+}
+
+# Create a managed instance group using the instance template
+resource "google_compute_instance_group_manager" "instance_group_manager" {
+  count              = var.create_template_vm ? 1 : 0
+  name               = "my-instance-group"
+  base_instance_name = "my-instance"
+  version {
+    instance_template = google_compute_instance_template.instance_template.id
+  }
+  zone        = var.zone
+  target_size = 1
+}
+
 
 
 # Create a Google Compute Engine instance
 resource "google_compute_instance" "my_first_vm" {
+  count        = var.create_standalone_vm ? 1 : 0
   name         = "my-first-vm"
   machine_type = var.machine_type
   zone         = var.zone
@@ -28,7 +80,7 @@ resource "google_compute_instance" "my_first_vm" {
   network_interface {
     network = "default"
     access_config {
-      # nat_ip = google_compute_address.static_ip.address
+      nat_ip = var.create_static_ip ? google_compute_address.static_ip[0].address : null
     }
   }
 
--- gce/gcp-vm/outputs.tf
+++ gce/gcp-vm/outputs.tf
@@ -1,7 +1,12 @@
 output "vm_instance_name" {
-  value = google_compute_instance.my_first_vm.name
+  value = length(google_compute_instance.my_first_vm) > 0 ? google_compute_instance.my_first_vm[0].name : null
 }
 
 output "vm_instance_ip" {
-  value = google_compute_instance.my_first_vm.network_interface[0].access_config[0].nat_ip
+  value = length(google_compute_instance.my_first_vm) > 0 ? google_compute_instance.my_first_vm[0].network_interface[0].access_config[0].nat_ip : null
 }
+
+output "static_ip_address" {
+  value = var.create_static_ip ? google_compute_address.static_ip[0].address : null
+}
+
