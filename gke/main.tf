1c04d1a042e7d26e96611789c19a273571c56d45
visheshofficial <21136428+visheshofficial@users.noreply.github.com>
2025-02-23 16:00:53 +0000

    Refactor GKE cluster configuration: add deletion protection, description, and remove unnecessary outputs

--- gke/main.tf
+++ gke/main.tf
@@ -4,23 +4,16 @@ provider "google" {
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "my-gke-cluster"
-  location = var.region
+  name                = "my-gke-cluster"
+  location            = var.region
+  deletion_protection = false
+  description         = "Demo cluster created during GCP Architect course"
 
-  initial_node_count = 1
-
-  node_config {
-    machine_type = var.machine_type
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/trace.append",
-    ]
-  }
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
 }
 
 resource "google_container_node_pool" "primary_nodes" {
@@ -32,6 +25,7 @@ resource "google_container_node_pool" "primary_nodes" {
   node_config {
     preemptible  = false
     machine_type = var.machine_type
+    disk_size_gb = 30 # Specify the disk size in GB
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/devstorage.read_only",
--- gke/output.tf
+++ gke/output.tf
@@ -5,15 +5,3 @@ output "cluster_name" {
 output "cluster_endpoint" {
   value = google_container_cluster.primary.endpoint
 }
-
-output "cluster_ca_certificate" {
-  value = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-}
-
-output "client_certificate" {
-  value = google_container_cluster.primary.master_auth.0.client_certificate
-}
-
-output "client_key" {
-  value = google_container_cluster.primary.master_auth.0.client_key
-}
